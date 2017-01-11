88039f5079776e53b5cc31e81f16aec345490a17
dextricyril <truitecendree@gmail.com>
2017-01-11 08:34:31 +0100

    Add string in the button creator methods

--- DesignPattern/Main.java
+++ DesignPattern/Main.java
@@ -1,3 +1,6 @@
+package Copain;
+
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 
 import javax.swing.JButton;
@@ -6,29 +9,31 @@ import javax.swing.JFrame;
 
 interface ButtonCreator {
 
-	public JButton createButton();
+	public JButton createButton(String text);
 	
 }
 
 
-class Wintype implements ButtonCreator
+class StarWarsTheme implements ButtonCreator
 {
-	public JButton createButton()
+	public JButton createButton(String text)
 	{
-		JButton win = new JButton("Star wars ?");
+		JButton win = new JButton(text);
 		win.setBounds(500,50,400, 50);
+		
 		return win;
 	}
 
 }
 
-class OtherType implements ButtonCreator
+class MarioTheme implements ButtonCreator
 {
 	JButton oth;
-	public JButton createButton()
+	public JButton createButton(String text)
 	{
-		 oth = new JButton("razmario");
+		 oth = new JButton(text);
 		 oth.setBounds(100,100,100,100);
+		 oth.setBackground(new Color(100,0,0));
 		return oth;
 	}
 
@@ -44,8 +49,8 @@ public class Main extends JFrame
 	{ 		
 		setSize(1000, 600);// set the size of the frame
 		
-		raz = new OtherType();// default enabled button is of razmario
-		myBeautifulButton=raz.createButton();
+		raz = new MarioTheme();// default enabled button is of razmario
+		myBeautifulButton=raz.createButton("default");
 		
 		mySelectButton=new JButton("Click me :)"); //add select button
 		mySelectButton.setBounds(500,300,300,50);
@@ -67,17 +72,17 @@ public class Main extends JFrame
 		    		getContentPane().validate();//repaint the scene with the previous button erased
 		    		getContentPane().repaint();
 		       
-		    	if(raz instanceof OtherType)
+		    	if(raz instanceof MarioTheme)
 		          {			    		
-		        	  raz = new Wintype();
-		        	  myBeautifulButton=raz.createButton();
+		        	  raz = new StarWarsTheme();
+		        	  myBeautifulButton=raz.createButton("LASERS");
 		      		add(myBeautifulButton);
 
 		          }
 		          else
 		          {
-		        	  raz = new OtherType();
-		        	  myBeautifulButton=raz.createButton();
+		        	  raz = new MarioTheme();
+		        	  myBeautifulButton=raz.createButton("ITALIAN");
 		      		 add(myBeautifulButton);
 
 		          }
